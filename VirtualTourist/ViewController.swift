fccde778b04833147418a0f30275b3851618a25d
Jan Gundorf <jgundorf@gnresound.com>
2020-05-18 20:31:25 +0200

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Jan Gundorf on 18/05/2020.
+//  Copyright © 2020 Jan Gundorf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VirtualTouristTests/VirtualTouristTests.swift
@@ -0,0 +1,34 @@
+//
+//  VirtualTouristTests.swift
+//  VirtualTouristTests
+//
+//  Created by Jan Gundorf on 18/05/2020.
+//  Copyright © 2020 Jan Gundorf. All rights reserved.
+//
+
+import XCTest
+@testable import VirtualTourist
+
+class VirtualTouristTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
